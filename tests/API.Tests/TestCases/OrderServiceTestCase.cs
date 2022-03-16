de913d0932709f6415931ec210c2f5fb64992a8d
Humberto Rodrigues <hpereira@senseilabs.com>
2022-03-16 01:50:17 -0400

    add stryker and fix some tests

---
+++ tests/API.Tests/TestCases/OrderServiceTestCase.cs
@@ -0,0 +1,39 @@
+﻿namespace PubDev.Store.API.Tests.TestCases
+{
+    public static class OrderServiceTestCase
+    {
+        public static IEnumerable<object[]> WithProductNotFoundAndWithInvalidQuantity = new[]
+        {
+            new object[]
+            {
+                new Order()
+                {
+                    ClientId = 1,
+                    OrderProducts = new List<OrderProduct>()
+                    {
+                        new OrderProduct()
+                        {
+                            ProductId = 1,
+                            Quantity = -1
+                        }
+                    }
+                }
+            },
+            new object[]
+            {
+                new Order()
+                {
+                    ClientId = 1,
+                    OrderProducts = new List<OrderProduct>()
+                    {
+                        new OrderProduct()
+                        {
+                            ProductId = 1,
+                            Quantity = 0
+                        }
+                    }
+                }
+            }
+        };
+    }
+}
